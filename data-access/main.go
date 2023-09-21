d16a9feccf562af0677c89cb8b5ff0a3a7c13543
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-21 14:39:13 +0400

    I created the first first file that will contain first the GO code to import the driver package that will translate requests I make through functions in the database/sql package into requests the database understands.

---
+++ data-access/main.go
@@ -0,0 +1,22 @@
+//import (
+//	"database/sql"
+//	"time"
+//
+//	_ "github.com/go-sql-driver/mysql"
+//)
+//
+// ...
+//
+// db, err := sql.Open("mysql", "user:password@/dbname")
+// if err != nil {
+//	panic(err)
+//}
+// See "Important settings" section.
+// db.SetConnMaxLifetime(time.Minute * 3)
+// db.SetMaxOpenConns(10)
+// db.SetMaxIdleConns(10)
+
+package main
+
+import "github.com/go-sql-driver/mysql"
+
